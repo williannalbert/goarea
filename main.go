d944c54d72cf514052bb96f98a92edd96907c4a8
Willian Nalbert <33140454+williannalbert@users.noreply.github.com>
2019-05-23 20:23:25 -0300

    Criação do arquivo main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package goarea
+
+import "math"
+
+const Pi = 3.1416
+
+//circ calculará a área da circunferencia
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+//Rect responsável por calcular a área de triangulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+//Não é visivel devido ao _
+func _Triangulo(base, altura float64) float64 {
+	return (base * altura) / 2
+}
