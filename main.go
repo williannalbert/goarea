769e342002f03aaba2edcb63d31a498dbc69ea44
Willian Nalbert <williannalbert123@gmail.com>
2019-06-26 21:05:11 -0300

    Feita a inclusão da frase teste "fim do programa"

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "math"
 
 const Pi = 3.1416
 
-//circ calculará a área da circunferencia
+//circ calculará a área da circunferencia.
 func Circ(raio float64) float64 {
 	return math.Pow(raio, 2) * Pi
 }
@@ -18,3 +18,5 @@ func Rect(base, altura float64) float64 {
 func _Triangulo(base, altura float64) float64 {
 	return (base * altura) / 2
 }
+
+fmt.println("Fim do programa")
